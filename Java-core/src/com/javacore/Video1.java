92c75438aa871a469d46c80a44750f54916284a7
0949876587 <mailoftrungdanh@gmail.com>
2023-03-10 10:30:21 +0700

    git lai

--- Java-core/src/com/javacore/Video1.java
+++ Java-core/src/com/javacore/Video1.java
@@ -8,10 +8,53 @@ public class Video1 {
 * git 
 * git clone https://github.com/vanthanh26021992/tutorial.git
 * git status
-* git add (teen file) - git add *.java
+* git add (teen file) - git add *.java 	- git add .
 * git commit -m "what is message"
+* git commit --amend -m "what is message" 	- Thêm vào 1 commit cũ vừa thêm
 * git push origin main
 * git pull origin main
+* git log --oneline 	- Kiểm tra git vắn tắt
+* git log --oneline - graph		- Kiểm tra lịch sử rẽ nhánh
+* git log  				- Kiểm tra git
+* mkdir tenthumuc 		- Tạo thư mục tên tenthumuc
+* cd tenthumuc/ 		- truy cap folder tenthumuc
+* code .   				- Mở vscode
+* code .\tenthumuc\		- Mở vscode
+* git init 				- Tạo ra một kho chứa git mới
+* git restore index.html- Khôi phục file index.html 
+* git restore .			- Khôi phục tất cả file đã xóa 
+* gif diff				- So sánh file cũ và file vừa sửa  
+* gif diff 25fd58d		- So sánh cm 25fd58d cũ và cm 25fd58d vừa sửa  
+* gif diff --staged		- So sánh toàn bộ sự thay đổi
+* git checkout 25fd58d -- index.html 	-Khôi phục file index.html về phiên bản  25fd58d
+* git checkout  -- index.html 	- Khôi phục file index.html về phiên bản  ngay trước
+* git reset --soft HEAD~1		- Xóa commit cuối đưa phần xóa vào vùng staged 
+* git reset --hard HEAD~1		- Xóa commit cuối không phục hồi được			
+* git reset --					- Xóa các file ở trong vùng staged - đưa file đó trở về vùng modified
+* git branch					- Kiểm tra xem trên dự án có những nhánh nào			
+* git branch alpha				- Tạo thêm 1 nhánh alpha	
+* git checkout -b alpha V1.0	- Tạo thêm 1 nhánh alpha tại tag V1.0	
+* git checkout alpha			- Chuyển sang làm việc trên nhánh alpha	
+* git switch master				- Chuyển sang làm việc trên nhánh master	
+* git merge sualoigap			- Gộp nhánh
+* git rebase sualoigap			- Gộp nhánh
+* git rebase continue			- Tiep tuc gộp nhánh
+* git branch -d sualoigap		- Xóa nhánh sualoigap 	
+* git remote add origin_githud địa chỉ		-giống git clone
+* git remote -v					- Kiểm tra các remote 
+* git push origin_githud master	- Đẩy dữ liệu lên kho chứa origin_githud nhánh master
+* git tag						- Đánh dấu cm
+* git tag -a "tentag" -m "ghi chu" tencm	- nếu ko điền tên cm thì sẽ là cm cuối cùng
+* git show tentag				- Xem chi tiết tag
+* git tag -d tentag				- Xóa tag
+* git push --delete origin tentag	- xóa tag trên remote (phải xóa trên local)
+* git fetch						- Cập nhật những thông tin mới từ remote
+* git push origin --tags		- Đẩy lên toàn bộ tag
+* gitk							- Hiển thị thông tin trực quan giúp quan sát lịch sử cm của nhánh
+* git gui						- Công cụ thực hiện cm 
+* Khi dùng lệnh có thể dùng tên tag để thay cho tên cm
+* Fork trên githud 	- sao chép kho chứa
+* pull request 		- yêu cầu cập nhật remote
 * 
 
 - own repository
