f8532be2e90969533dd2a5436544973ac57a8b77
0949876587 <mailoftrungdanh@gmail.com>
2023-05-01 22:14:14 +0700

    luu my-app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,59 +1,78 @@
 import Todolist from "./components/Todolist";
 import { useCallback, useEffect, useState } from "react";
-import { v4} from 'uuid';
-import { Button } from 'primereact/button';
-import { InputText } from 'primereact/inputtext';
-import ComponentDemo from "./components/ComponentDemo";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import axios from "axios";
 
-const TODO_APP_STORAGE_KEY = 'TODO_APP';
+const TODO_APP_STORAGE_KEY = "TODO_APP";
 
 function App() {
   // props: truyền dữ liệu từ bên ngoài vào component
-  // html, css, javascript: 
+  // html, css, javascript:
   const [todoList, setTodoList] = useState([]);
-  const [textInput, setTextInput] = useState("");
+  const [textInputId, setTextInputId] = useState("");
+  const [textInputName, setTextInputName] = useState("");
 
-  useEffect(() =>{
+  useEffect(() => {
     const storagedTodoList = localStorage.getItem(TODO_APP_STORAGE_KEY);
     if (storagedTodoList) {
       setTodoList(JSON.parse(storagedTodoList));
     }
   }, []);
-  
+
   useEffect(() => {
     localStorage.setItem(TODO_APP_STORAGE_KEY, JSON.stringify(todoList));
-  },[todoList]);
+  }, [todoList]);
 
+  const onAddBtnClick = useCallback(
+    (e) => {
+      // Them text input vao danh sach todoLish
+      // setTodoList([
+      //   { id: textInputId, name: textInputName, isCompleted: false },
+      //   ...todoList,
+      // ]);
 
-  const onAddBtnClick = useCallback((e) => {
-    // Them text input vao danh sach todoLish
-    setTodoList([
-      {id:v4(), name: textInput, isCompleted: false},
-      ...todoList,
-    ]);
-    setTextInput("");
-    console.log(todoList);
-  },[textInput, todoList]
+      setTextInputId();
+      setTextInputName("");
+      console.log(textInputId);
+      console.log(textInputName);
+      const headers = { "Content-Type": "application/json;charset=utf-8" };
+      axios
+        .post(
+          `http://localhost:8082/insert`,
+          JSON.stringify({ id: textInputId, name: textInputName || "" }),
+          { headers }
+        )
+        .catch((error) => console.log(error));
+    },
+    [textInputId, textInputName]
   );
 
-  const onCheckBtnClick = useCallback((id) =>{
-  setTodoList((prevState) => prevState.map(todo => 
-    todo.id ===id ? {...todo, isCompleted: true} : todo));
+  const onCheckBtnClick = useCallback((id) => {
+    setTodoList((prevState) =>
+      prevState.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: true } : todo
+      )
+    );
   }, []);
 
-  
   return (
     <>
       <h3>Danh sách cần làm</h3>
-      Tên việc: <InputText value={textInput} onChange={(e) => setTextInput(e.target.value)} />
-      Thời gian làm: <InputText value={textInput} onChange={(e) => setTextInput(e.target.value)} />
-
-       <Button label="Them" onClick={onAddBtnClick}/>
+      ID:{" "}
+      <InputText
+        value={textInputId || ""}
+        onChange={(e) => setTextInputId(e.target.value)}
+      />
+      Ten:{" "}
+      <InputText
+        value={textInputName || ""}
+        onChange={(e) => setTextInputName(e.target.value)}
+      />
+      <Button label="Them" onClick={onAddBtnClick} />
       <Todolist todoList={todoList} onCheckBtnClick={onCheckBtnClick} />
-      <ComponentDemo />
     </>
   );
 }
 
 export default App;
-
--- my-app/src/components/Todolist.js
+++ my-app/src/components/Todolist.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { Button } from "primereact/button";
@@ -7,53 +7,105 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import { Dialog } from "primereact/dialog";
 import { InputText } from "primereact/inputtext";
+import axios from "axios";
 
 export default function Todolist({ todoList, onCheckBtnClick }) {
+  const [visible, setVisible] = useState(false);
   const [visibleEdit, setVisibleEdit] = useState(false);
+  const [idInput, setIdInput] = useState("");
   const [nameInput, setNameInput] = useState("");
   const [datetimeInput, setDatetimeInput] = useState("");
   const [selectedTodo, setSelectedTodo] = useState({});
+  const [data, setData] = useState({});
 
-  const [data, setData] = useState([
-    {
-      id: 1,
-      name: "Name 1",
-      datetime: "2023-04-12 12:10",
-      status: "Done",
-    },
-    {
-      id: 2,
-      name: "Wash dish",
-      datetime: "2023-04-12 2:10",
-      status: "Doing",
-    },
-  ]);
+  useEffect(() => {
+    // edit: select by id => edit
+    // delete : delete
+    axios
+      // put - post
+      .get(`http://localhost:8082/select`)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, [data]);
 
   const onClickEdit = (todo) => {
     console.log("onclick edit");
     setVisibleEdit(true);
+    setIdInput(todo.id);
     setNameInput(todo.name);
     setDatetimeInput(todo.datetime);
     setSelectedTodo(todo);
+    console.log(todo.id);
+    console.log(todo.name);
+    console.log(idInput);
+    console.log(nameInput);
   };
 
-  const updateTodo = () => {
-    data.forEach((element) => {
-      if (element.id === selectedTodo.id) {
-        element.name = nameInput;
-        element.datetime = datetimeInput;
-      }
-    });
+  const updateTodo = (todo) => {
+    console.log(idInput);
+    console.log(nameInput);
+
+    const headers = { "Content-Type": "application/json;charset=utf-8" };
+    axios
+      .put(
+        `http://localhost:8082/update`,
+        JSON.stringify({ id: idInput, name: nameInput }),
+        { headers }
+      )
+      .catch((error) => console.log(error));
+
     setData(data);
     console.log(data);
     setVisibleEdit(false);
+  };
+
+  const footerContent = (
+    <div>
+      <Button
+        label="No"
+        icon="pi pi-times"
+        onClick={() => setVisible(false)}
+        className="p-button-text"
+      />
+      <Button
+        label="Yes"
+        icon="pi pi-check"
+        onClick={(todo) => {
+          setIdInput(todo.id);
+          setNameInput(todo.name);
+          setDatetimeInput(todo.datetime);
+          setSelectedTodo(todo);
+          axios
+            .delete(`http://localhost:8082/deleteId/${idInput}`)
+            .then((res) => {
+              console.log(res.data);
+              setData(res.data);
+            })
+            .catch((error) => console.log(error));
+
+          setVisible(false);
+        }}
+        autoFocus
+      />
+    </div>
+  );
+
+  const onClickDelete = (todo) => {
+    console.log("onclick delete");
+    setVisible(true);
+    setIdInput(todo.id);
+    setNameInput(todo.name);
+    setDatetimeInput(todo.datetime);
+    setSelectedTodo(todo);
   };
 
   const actionBodyTemplate = (todo) => {
     return (
       <>
         <Button
-          icon="pi-delete-left"
+          icon="pi pi-delete-left"
           rounded
           text
           aria-label="Filter"
@@ -61,11 +113,13 @@ export default function Todolist({ todoList, onCheckBtnClick }) {
           onClick={() => onClickEdit(todo)}
         ></Button>
         <Button
-          icon="pi-file-edit"
+          icon="pi pi-file-edit"
           rounded
           text
           aria-label="Filter"
           label="Delete"
+          onClick={() => onClickDelete(todo)}
+          autoFocus
         ></Button>
       </>
     );
@@ -74,6 +128,7 @@ export default function Todolist({ todoList, onCheckBtnClick }) {
   return (
     <>
       <DataTable value={data} tableStyle={{ minWidth: "50rem" }}>
+        <Column field="id" header="Id"></Column>
         <Column field="name" header="Name"></Column>
         <Column field="datetime" header="Datetime"></Column>
         <Column field="status" header="Status"></Column>
@@ -85,24 +140,46 @@ export default function Todolist({ todoList, onCheckBtnClick }) {
         style={{ width: "50vw" }}
         onHide={() => setVisibleEdit(false)}
       >
-        Tên việc:{" "}
+        ID:{" "}
+        <InputText
+          value={idInput}
+          onChange={(e) => setIdInput(e.target.value)}
+        />
+        Tên:{" "}
         <InputText
           value={nameInput}
           onChange={(e) => setNameInput(e.target.value)}
         />
-        Thời gian làm:{" "}
-        <InputText
-          value={datetimeInput}
-          onChange={(e) => setDatetimeInput(e.target.value)}
-        />
         <Button
-          icon="pi-file-edit"
+          icon="pi pi-file-edit"
           label="Update"
           onClick={updateTodo}
         ></Button>
       </Dialog>
+
+      <Dialog
+        header="Header"
+        visible={visible}
+        style={{ width: "50vw" }}
+        onHide={() => setVisible(false)}
+        footer={footerContent}
+      ></Dialog>
     </>
   );
 }
 
 //json
+// const [data, setData] = useState([
+//   {
+//     id: 1,
+//     name: "Name 1",
+//     datetime: "2023-04-12 12:10",
+//     status: "Done",
+//   },
+//   {
+//     id: 2,
+//     name: "Wash dish",
+//     datetime: "2023-04-12 2:10",
+//     status: "Doing",
+//   },
+// ]);
